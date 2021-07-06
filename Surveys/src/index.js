9c99cef3101bbc2da8897d18ad6e1c593fe81871
unknown <razkaziro@gmail.com>
2021-07-06 10:45:41 +0300

    auth0 implement

--- Surveys/src/index.js
+++ Surveys/src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-surveys.eu.auth0.com"
-    clientId="2DH0f8KYcUbDeW4UEuD0CH73MUfgeJCv"
-    redirectUri={window.location.origin}
-  >
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}>
+    console.log({clientId});
     <App />
   </Auth0Provider>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
